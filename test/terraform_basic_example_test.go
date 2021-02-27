2fb856ea508bdc7d899b6c8289150cc7708dbd3b
ASC IT Solutions <andrew@asc-solutions.co.uk>
2021-02-27 11:25:49 +0000

    initial commit

--- test/terraform_basic_example_test.go
+++ test/terraform_basic_example_test.go
@@ -18,7 +18,7 @@ func TestTerraformBasicExample(t *testing.T) {
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// website::tag::1::Set the path to the Terraform code that will be tested.
 		// The path to where our Terraform code is located
-		TerraformDir: "../examples/terraform-basic-example",
+		TerraformDir: "../examples",
 
 		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
@@ -33,7 +33,7 @@ func TestTerraformBasicExample(t *testing.T) {
 		VarFiles: []string{"varfile.tfvars"},
 
 		// Disable colors in Terraform commands so its easier to parse stdout/stderr
-		NoColor: true,
+		NoColor: false,
 	})
 
 	// website::tag::4::Clean up resources with "terraform destroy". Using "defer" runs the command at the end of the test, whether the test succeeds or fails.
